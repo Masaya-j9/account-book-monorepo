1a6634c58633cd434f999e9d91f340e37943b2b1
Masaya-j9 <masaya.spectre@gmail.com>
2025-12-29 22:08:45 +0900

    fmtの修正

--- apps/backend/src/controller/routers/openapi/index.ts
+++ apps/backend/src/controller/routers/openapi/index.ts
@@ -1,25 +1,25 @@
-import type { NodePgDatabase } from "@account-book-app/db";
-import { swaggerUI } from "@hono/swagger-ui";
-import type { OpenAPIHono } from "@hono/zod-openapi";
+import type { NodePgDatabase } from '@account-book-app/db';
+import { swaggerUI } from '@hono/swagger-ui';
+import type { OpenAPIHono } from '@hono/zod-openapi';
 
-import { registerCategoriesOpenApi } from "./categories.openapi";
+import { registerCategoriesOpenApi } from './categories.openapi';
 
 export const registerOpenApi = (app: OpenAPIHono, db: NodePgDatabase) => {
-	app.doc("/openapi.json", (c) => ({
-		openapi: "3.0.0",
-		info: {
-			version: "1.0.0",
-			title: "Account Book API",
-		},
-		servers: [
-			{
-				url: new URL(c.req.url).origin,
-				description: "Current environment",
-			},
-		],
-	}));
+  app.doc('/openapi.json', (c) => ({
+    openapi: '3.0.0',
+    info: {
+      version: '1.0.0',
+      title: 'Account Book API',
+    },
+    servers: [
+      {
+        url: new URL(c.req.url).origin,
+        description: 'Current environment',
+      },
+    ],
+  }));
 
-	app.get("/doc", swaggerUI({ url: "/openapi.json" }));
+  app.get('/doc', swaggerUI({ url: '/openapi.json' }));
 
-	registerCategoriesOpenApi(app, db);
+  registerCategoriesOpenApi(app, db);
 };
