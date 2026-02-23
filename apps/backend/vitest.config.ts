4e7721eaea126f355da4f8d59398495fa1c624b2
Masaya-j9 <masaya.spectre@gmail.com>
2026-02-23 18:17:58 +0900

    設定の変更

--- apps/backend/vitest.config.ts
+++ apps/backend/vitest.config.ts
@@ -1,16 +1,17 @@
-import path from 'node:path';
-import { defineConfig } from 'vitest/config';
+import path from "node:path";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    globals: true,
-  },
-  resolve: {
-    alias: {
-      '@account-book-app/shared': path.resolve(
-        __dirname,
-        '../../packages/shared/src',
-      ),
-    },
-  },
+	test: {
+		globals: true,
+	},
+	resolve: {
+		alias: {
+			"@account-book-app/db": path.resolve(__dirname, "../../packages/db/src"),
+			"@account-book-app/shared": path.resolve(
+				__dirname,
+				"../../packages/shared/src",
+			),
+		},
+	},
 });
